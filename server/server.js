e23f8afa2a4d6e29e7606831c6dbf792544e9b20
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-16 11:13:49 -0400

    connected mongoose and the mongodb, setup dotenv

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -18,9 +21,21 @@ app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+const uri = process.env.DATABASE_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
