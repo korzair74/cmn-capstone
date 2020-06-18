a3b54e6f94f434644eb41c73e01489fec401494a
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-18 11:23:17 -0400

    mapped data and started form

--- client/src/Components/Pages/tamingCalc.js
+++ client/src/Components/Pages/tamingCalc.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function TamingCalc() {
+  useEffect(() => {
+    fetchTamingStats();
+  }, []);
+
+  const [tamingStats, setTamingStats] = useState([]);
+
+  const fetchTamingStats = async () => {
+    const data = await fetch("http://localhost:5000/tamingStats");
+    const tamingStats = await data.json();
+    setTamingStats(tamingStats);
+    console.log(tamingStats);
+  };
+
   return (
     <div className='tamingCalc-container'>
       <div className='calc-wrapper'>
         <div className='calc-details-wrapper'>
           <form>
-            <input type='' />
+            <select name='creature' className='select-element'>
+              {tamingStats.map((stat) => (
+                <option key={stat.id} value={stat.creature}>
+                  {stat.creature}
+                </option>
+              ))}
+            </select>
             <li>Creature</li>
             <li>Level</li>
             <li>Taming Method</li>
