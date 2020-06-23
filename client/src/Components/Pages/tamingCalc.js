a0121c2c9ae9345a84be4c32104904c1883e90d4
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-23 18:17:12 -0400

    Created CreatureDetails component

--- client/src/Components/Pages/tamingCalc.js
+++ client/src/Components/Pages/tamingCalc.js
@@ -1,109 +1,107 @@
 import React, { useState, useEffect } from "react";
+import CreatureDetails from "../creatureDetails";
 
 export default function TamingCalc() {
   useEffect(() => {
-    fetchTamingStats();
+    fetchArkData();
   }, []);
 
-  const [tamingStats, setTamingStats] = useState([]);
+  const [arkData, setArkData] = useState([]);
+  const [creature, setCreature] = useState("Argentavis");
+  const [level, setLevel] = useState("1");
+  const [creatureDetails, setCreatureDetails] = useState([]);
 
-  const fetchTamingStats = async () => {
-    const data = await fetch("http://localhost:5000/tamingStats");
-    const tamingStats = await data.json();
-    setTamingStats(tamingStats);
-    console.log(tamingStats);
+  const fetchArkData = async () => {
+    const data = await fetch("http://localhost:5000/arkdata");
+    const arkData = await data.json();
+    console.log(arkData);
+    setArkData(arkData);
+    setCreatureDetails(arkData[0].levelOne);
   };
 
-  const headings = ["Food Type", "Qty", "Time"];
-  const rows = [
-    "Kibble",
-    "Crops",
-    "Mejoberry",
-    "Berries",
-    "Raw Mutton",
-    "Raw Prime Meat",
-    "Cooked Lamb Chop",
-    "Cooked Prime Meat",
-    "Raw Prime Fish Meat",
-    "Raw Meat",
-    "Cooked Meat",
-  ];
+  useEffect(() => {
+    // call your switch statement and pass the level
+    console.log("level changed!");
+  }, [level]);
+  // levelSelector = () => {
+  //   switch ({ level }) {
+  //     case { level } < 30:
+  //       return `${stat.levelOne}`;
+  //     case { level } < 60:
+  //       return `${stat.levelThirty}`;
+  //     case { level } < 90:
+  //       return `${stat.levelSixty}`;
+  //     case { level } < 120:
+  //       return `${stat.levelNinety}`;
+  //     case { level } < 150:
+  //       return `${stat.levelOneHundredTwenty}`;
+  //     case { level } >= 150:
+  //       return `${stat.levelOneHundredFifty}`;
+  //   }
+  // };
 
   return (
     <form>
       <div className='taming-calc-container'>
         <div className='taming-comp-wrapper'>
-          <div className='taming-components'>
-            <div className='calc-wrapper'>
-              <div className='calc-form-wrapper'>
-                <div>
-                  <h5>
-                    Creature:&emsp;
-                    <select name='creature' className='select-element'>
-                      {tamingStats.map((stat) => (
-                        <option
-                          className='select-option'
-                          key={stat.id}
-                          value={stat.creature}>
-                          {stat.creature}
-                        </option>
-                      ))}
-                    </select>
-                  </h5>
-                </div>
-                <div>
-                  <h5>
-                    Creature Level:&emsp;
-                    <input
-                      type='number'
-                      className='select-number'
-                      defaultValue='1'
-                    />
-                  </h5>
-                </div>
-                {/* <div>
-                  <h5>
-                    <input
-                      type='radio'
-                      value='Standard'
-                      name='method'
-                      className='radio-element'
-                    />
-                    <label for='method'>Standard</label>
-                    <input
-                      type='radio'
-                      value='Starve'
-                      name='method'
-                      className='radio-element'
-                    />
-                    <label for='method'>Starve</label>
-                  </h5>
-                </div> */}
-                <div>
-                  <h5>
-                    Taming Multiplier:&emsp;
-                    <input
-                      type='number'
-                      defaultValue='1'
-                      className='select-number'
-                    />
-                  </h5>
-                </div>
-                <div>
-                  <h5>
-                    Consumption Multiplier:&emsp;
-                    <input
-                      type='number'
-                      defaultValue='1'
-                      className='select-number'
-                    />
-                  </h5>
-                </div>
+          <div className='calc-wrapper'>
+            <div className='calc-form-wrapper'>
+              <div>
+                <h5>
+                  Creature:&emsp;
+                  <select
+                    name='creature'
+                    className='select-element'
+                    onChange={(e) => setCreature(e.target.value)}>
+                    {arkData.map((stat) => (
+                      <option
+                        className='select-option'
+                        key={stat._id}
+                        value={stat.name}>
+                        {stat.name}
+                      </option>
+                    ))}
+                  </select>
+                </h5>
+              </div>
+              <div>
+                <h5>
+                  Creature Level:&emsp;
+                  <input
+                    type='number'
+                    className='select-number'
+                    onChange={(e) => setLevel(e.target.value)}
+                    value={level}
+                  />
+                </h5>
+              </div>
+              <div>
+                <h5>
+                  Taming Multiplier:&emsp;
+                  <input
+                    type='number'
+                    defaultValue='1'
+                    className='select-number'
+                  />
+                </h5>
+              </div>
+              <div>
+                <h5>
+                  Consumption Multiplier:&emsp;
+                  <input
+                    type='number'
+                    defaultValue='1'
+                    className='select-number'
+                  />
+                </h5>
               </div>
             </div>
-            <div className='food-details-wrapper'>
-              <table></table>
-            </div>
+          </div>
+
+          <div className='details-wrapper'>
+            {creatureDetails.map((creatureDetail) => {
+              return <CreatureDetails creatureDetail={creatureDetail} />;
+            })}
           </div>
         </div>
       </div>
---
+++ client/src/Components/creatureDetails.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function CreatureDetails(props) {
+  return (
+    <div>
+      <h1>Hello from CreatureDetails</h1>
+    </div>
+  );
+}
+
+{
+  /* <div className='detail-wrapper'>
+<div>
+  <h5>
+    Food Type:&emsp;
+    {}
+  </h5>
+</div>
+<div>
+  <h5>
+    Creature Level:&emsp;
+    <input
+      type='number'
+      className='select-number'
+      defaultValue='1'
+    />
+  </h5>
+</div>
+<div>
+  <h5>
+    Taming Multiplier:&emsp;
+    <input
+      type='number'
+      defaultValue='1'
+      className='select-number'
+    />
+  </h5>
+</div>
+<div>
+  <h5>
+    Consumption Multiplier:&emsp;
+    <input
+      type='number'
+      defaultValue='1'
+      className='select-number'
+    />
+  </h5>
+</div>
+</div> */
+}
