f1fa8321cc32703eb41cfc423c2016f8a4196945
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-25 14:24:38 -0400

    deployment bugs

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 const tamingStatsRouter = require("./routes/tamingStats");
 const breedingStatsRouter = require("./routes/breedingStats");
 const arkdataRouter = require("./routes/arkdata");
-const port = process.env.PORT || "5000";
+const PORT = process.env.PORT || "5000";
 
 const app = express();
 
@@ -36,23 +36,23 @@ app.use("/tamingStats", tamingStatsRouter);
 app.use("/breedingStats", breedingStatsRouter);
 app.use("/arkdata", arkdataRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.listen(port, console.log(`Express server is running on ${port}`));
+app.listen(PORT, console.log(`Express server is running on ${PORT}`));
 module.exports = app;
