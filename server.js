b6b0ebd21b19ec9f0fff315e50e9f99ce85c16f9
Chris Nickel <chris.nickel.dev@gmail.com>
2020-06-25 12:04:37 -0400

    refactoring for deployment

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ const port = process.env.PORT || "5000";
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
